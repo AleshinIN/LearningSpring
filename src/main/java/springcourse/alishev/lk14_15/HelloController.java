818786e0b8386baee103fea72076b20cd5034da1
AleshinIN <ivann9988@yandex.ru>
2022-09-18 22:46:54 +0300

    Создание MVC с помощью xml(lk14-15) и java-class(lk16). lk13

---
+++ src/main/java/springcourse/alishev/lk14_15/HelloController.java
@@ -0,0 +1,14 @@
+//package springcourse.alishev.lk14_15;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///** Запускаем MVC через XML-файл */
+//@Controller
+//public class HelloController {
+//
+//    @GetMapping("/hello-world") // какой URL будет приходить в этот метод контроллера(по какому адресу должен перейти клиент)
+//    public String sayHello() {
+//        return "hello_world"; // скорее всего открывает hello_world.html(в appCont м указали адрес "/WEB-INF/views/")
+//    }
+//}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!! Стартовая страница - index.jsp</h2>
 </body>
 </html>
